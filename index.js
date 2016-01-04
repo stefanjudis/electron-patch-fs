2ec0e76ba993be1690779415dd5fbe71fd6354f7
stefan judis <stefanjudis@gmail.com>
2016-01-04 21:19:48 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,73 @@
+'use strict';
+
+let originals = require( './original/index' );
+
+
+/**
+ * Modules and functions to be patched
+ * 
+ * @type {Array}
+ */
+const PATCHES = [
+  {
+    name : 'fs',
+    fns  : [ 'open', 'openSync' ]
+  }
+];
+
+
+/**
+ * Object cache for electron functions
+ *
+ * @type {Object}
+ */
+let cache = {};
+
+
+/**
+ * Monkey patch the functions that were
+ * changed by electron
+ */
+function patch() {
+  PATCHES.forEach( function( patch ) {
+    var module = require( patch.name );
+
+    patch.fns.forEach( function( fn ) {
+      if ( ! cache[ patch.name ] ) {
+        cache[ patch.name ] = {};
+      }
+
+      cache[ patch.name ][ fn ] = module[ fn ];
+
+      module[ fn ] = originals[ patch.name ][ fn ];
+    } );
+  } );
+}
+
+
+/**
+ * Undo monkey patch of the functions that were
+ * changed by electron
+ */
+function unpatch() {
+  if ( ! Object.keys( cache ).length ) {
+    throw new Error(
+      'Nothing to unpatch. Make sure you called `patch` before'
+    );
+  }
+
+  PATCHES.forEach( function( patch ) {
+    var module = require( patch.name );
+
+    patch.fns.forEach( function( fn ) {
+      module[ fn ] = cache[ patch.name ][ fn ];
+    } );
+  } );
+
+  cache = {};
+}
+
+module.exports = {
+  patch   : patch,
+  unpatch : unpatch
+};
---
+++ index.spec.js
@@ -0,0 +1,31 @@
+import test from 'ava';
+import patcher from './index';
+import fs from 'fs';
+
+let originalOpen;
+
+test.before( () => {
+  originalOpen = fs.open;
+
+  fs.open = function() {};
+} );
+
+test( 'patch', function( t ) {
+  patcher.patch();
+
+  t.notSame( fs.open.toString(), 'function () {}' );
+} );
+
+test( 'unpatch', function( t ) {
+  patcher.unpatch();
+
+  t.same( fs.open.toString(), 'function () {}' );
+
+  t.throws( function() {
+    patcher.unpatch();
+  } );
+} );
+
+test.after( () => {
+  fs.open = originalOpen;
+} );
